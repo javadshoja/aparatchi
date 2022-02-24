81feb90b7a08565afaf940d04629b2a59a094b15
jbrave <jbraave.gmail.com>
2022-02-24 12:42:40 +0330

    feat(server): add errorHandler middleware

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import 'colorts/lib/string'
 import express from 'express'
+import { errorHandler } from '~/middleware'
 
 const PORT = process.env.PORT || 4000
 
@@ -8,6 +9,8 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
 	console.log(`🚀 Server is running at http://localhost:${PORT}`.cyan)
