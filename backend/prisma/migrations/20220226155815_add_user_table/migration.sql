2ee90c1d2992869cd1712ec9ca33e971651f8058
jbrave <jbraave.gmail.com>
2022-02-26 19:40:28 +0330

    feat(schema): add User model

---
+++ backend/prisma/migrations/20220226155815_add_user_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
