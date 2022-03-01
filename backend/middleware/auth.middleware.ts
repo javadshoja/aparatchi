4652dac5c414683b33aa7558e4247de85ed432e2
ijbrave <ssjavad00@gmail.com>
2022-03-01 20:25:59 +0330

    Add auth middleware
    setup protect for private routes with jwt

---
+++ backend/middleware/auth.middleware.ts
@@ -0,0 +1,60 @@
+import asyncHandler from 'express-async-handler'
+import type { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+import { PrismaClient } from '@prisma/client'
+
+export interface IuserRequest extends Request {
+	user?: any
+}
+
+interface JwtPayload {
+	id: string
+}
+
+const { user } = new PrismaClient()
+
+const protect = asyncHandler(
+	async (req: IuserRequest, res: Response, next: NextFunction) => {
+		let token
+
+		const { authorization } = req.headers
+
+		if (authorization && authorization.startsWith('Bearer')) {
+			try {
+				// Get token from headers
+				token = authorization.split(' ')[1]
+
+				console.log(token)
+
+				// Verify token
+				const decoded = verify(
+					token,
+					String(process.env.JWT_SECRET),
+				) as JwtPayload
+
+				req.user = await user.findUnique({
+					where: {
+						id: decoded.id,
+					},
+					select: {
+						id: true,
+						name: true,
+						email: true,
+					},
+				})
+				next()
+			} catch (error) {
+				console.log(error)
+				res.status(401)
+				throw new Error('Not authorize')
+			}
+		}
+
+		if (!token) {
+			res.status(401)
+			throw new Error('Not authorize, no token')
+		}
+	},
+)
+
+export default protect
--- backend/middleware/index.ts
+++ backend/middleware/index.ts
@@ -1,1 +1,2 @@
 export { default as errorHandler } from './error.middleware'
+export { default as protect, IuserRequest } from './auth.middleware'
