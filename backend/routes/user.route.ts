24938cb7f3f05f359951e6fea57f4b1bc28951d7
ijbrave <ssjavad00@gmail.com>
2022-03-01 20:29:00 +0330

    feat(user.route): add protect for getMe

--- backend/routes/user.route.ts
+++ backend/routes/user.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { registerUser, loginUser, getMe } from '~/controllers'
+import { protect } from '~/middleware'
 
 const router = Router()
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.get('/me', getMe)
+router.get('/me', protect, getMe)
 
 export default router
