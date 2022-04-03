164d2f576a16a305c2d77462df1e715aa8be3427
ijbrave <ssjavad00@gmail.com>
2022-04-03 20:03:37 +0430

    feat(routes): add name input to register page

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -8,15 +8,28 @@ import {
 } from '~/components/styles/Form.styled'
 import { FormArea, ImageArea } from '~/components/styles/Register.styled'
 import { ButtonBlock } from '~/components/styles'
+import styled from '@emotion/styled'
+
+const Container = styled(InputContainer)`
+	margin-top: 20px;
+	margin-bottom: 20px;
+`
+
+const RegisterHeader = styled(Header)`
+	p {
+		margin: 10px 0 30px;
+	}
+`
 
 function Register() {
 	const [formData, setFormData] = useState({
+		name: '',
 		email: '',
 		password: '',
 		password2: ''
 	})
 
-	const { email, password, password2 } = formData
+	const { name, email, password, password2 } = formData
 
 	const onChange = (e: any) => {
 		setFormData(prevState => ({
@@ -32,13 +45,23 @@ function Register() {
 					<Img src='images/register.svg' alt='register' />
 				</ImageArea>
 				<FormArea>
-					<Header>
+					<RegisterHeader>
 						<h1>:) خوش اومدی</h1>
 						<p>ثبت‌نام و ساخت حساب کاربری</p>
-					</Header>
+					</RegisterHeader>
 
 					<form method='POST'>
-						<InputContainer>
+						<Container>
+							<Input
+								type='name'
+								name='name'
+								id='name'
+								placeholder='نام کاربری'
+								value={name}
+								onChange={onChange}
+							/>
+						</Container>
+						<Container>
 							<Input
 								type='email'
 								name='email'
@@ -47,8 +70,8 @@ function Register() {
 								value={email}
 								onChange={onChange}
 							/>
-						</InputContainer>
-						<InputContainer>
+						</Container>
+						<Container>
 							<Input
 								type='password'
 								name='password'
@@ -57,8 +80,8 @@ function Register() {
 								value={password}
 								onChange={onChange}
 							/>
-						</InputContainer>
-						<InputContainer>
+						</Container>
+						<Container>
 							<Input
 								type='password2'
 								name='password2'
@@ -67,10 +90,10 @@ function Register() {
 								value={password2}
 								onChange={onChange}
 							/>
-						</InputContainer>
-						<InputContainer>
+						</Container>
+						<Container>
 							<ButtonBlock type='submit'>ثبت نام</ButtonBlock>
-						</InputContainer>
+						</Container>
 					</form>
 				</FormArea>
 			</Grid>
