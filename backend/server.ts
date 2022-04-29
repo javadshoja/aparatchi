c25a827bf62d408c0797bb95c72b716f59c2ab54
ijbrave <ssjavad00@gmail.com>
2022-04-29 11:13:59 +0430

    Add cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import 'colorts/lib/string'
 import express from 'express'
+import cors from 'cors'
 import movie from '~/routes/movie.route'
 import user from '~/routes/user.route'
 import { errorHandler } from '~/middleware'
@@ -11,6 +12,8 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use(cors())
 
 app.use('/api/movie', movie)
 app.use('/api/user', user)
