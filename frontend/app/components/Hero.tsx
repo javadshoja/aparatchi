f30177838363b08ec19680d6fc3e0a7a5e7efbb5
ijbrave <ssjavad00@gmail.com>
2022-04-29 11:43:27 +0430

    Add Hero component

---
+++ frontend/app/components/Hero.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled'
+import { useNavigate } from 'remix'
+import { Button } from './styles'
+
+function Hero() {
+	const navigate = useNavigate()
+	function onClick() {
+		navigate('/login')
+	}
+	return (
+		<>
+			<Container>
+				<Main>
+					<h2>!هنوز وارد نشده اید</h2>
+					<p>.برای دسترسی به محتوای سایت لطفا وارد شوید</p>
+					<Button onClick={onClick}>ورود به سایت</Button>
+				</Main>
+			</Container>
+		</>
+	)
+}
+
+export default Hero
+
+const Container = styled.section`
+	display: grid;
+	align-items: center;
+	height: 85vh;
+	grid-template-columns: repeat(12, 1fr);
+	grid-template-rows: repeat(12, 1fr);
+`
+const Main = styled.div`
+	grid-column: 5/9;
+	grid-row: 1/10;
+	display: grid;
+	text-align: center;
+	gap: 1rem;
+
+	@media (max-width: 60em) {
+		grid-column: 4/10;
+	}
+	@media (max-width: 40em) {
+		grid-column: 3/11;
+	}
+	@media (max-width: 30em) {
+		grid-column: 2/12;
+	}
+`
--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,3 +1,27 @@
+import { LoaderFunction, redirect, useLoaderData } from 'remix'
+import { Hero } from '~/components'
+import { getUser } from '~/utils/session.server'
+
+export const loader: LoaderFunction = async ({ request }) => {
+	const user = await getUser(request)
+
+	return { user }
+}
+
 export default function Index() {
-	return <h1>Hello world</h1>
+	const { user } = useLoaderData()
+
+	return (
+		<>
+			{user ? (
+				<>
+					<h3>{user.name} سلام</h3>
+				</>
+			) : (
+				<>
+					<Hero />
+				</>
+			)}
+		</>
+	)
 }
