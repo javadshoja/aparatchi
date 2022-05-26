4bb9c97a49248e57b2773d08270f7b655f224bf5
ijbrave <ssjavad00@gmail.com>
2022-05-26 12:41:45 +0430

    fix Header position

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -3,18 +3,66 @@ import { Link as link } from 'remix'
 import { IoLogInOutline } from 'react-icons/io5'
 import { AiOutlineUserAdd } from 'react-icons/ai'
 import { Button } from './styles'
+import { useEffect, useState } from 'react'
+
+function Header({ user }: any) {
+	const [isScrolled, setIsScrolled] = useState(false)
+
+	useEffect(() => {
+		function handelScroll() {
+			if (window.scrollY > 0) setIsScrolled(true)
+			else setIsScrolled(false)
+		}
+
+		window.addEventListener('scroll', handelScroll)
+
+		return () => {
+			window.removeEventListener('scroll', handelScroll)
+		}
+	}, [])
+	return (
+		<HeaderContainer className={`${isScrolled && 'enable-scroll'}`}>
+			<Logo to='/'>Aparatchi</Logo>
+			<nav>
+				<Items>
+					{user ? (
+						<Item>
+							<form action='/auth/logout' method='POST'>
+								<Button type='submit'>{user.name} خروج از حساب</Button>
+							</form>
+						</Item>
+					) : (
+						<>
+							<Item>
+								<Link to='/login'>
+									ورود <IoLogInOutline />
+								</Link>
+							</Item>
+							<Item>
+								<Link to='/register'>
+									ثبت نام <AiOutlineUserAdd />
+								</Link>
+							</Item>
+						</>
+					)}
+				</Items>
+			</nav>
+		</HeaderContainer>
+	)
+}
+
+export default Header
 
 const HeaderContainer = styled.header`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+	width: 100%;
 	padding: 20px 5%;
-	position: sticky;
+	position: fixed;
 	top: 0;
 	z-index: 999;
 `
-
-const Navbar = styled.nav``
 
 const Items = styled.ul`
 	display: flex;
@@ -41,37 +89,3 @@ const Logo = styled(Link)`
 	font-weight: 600;
 	color: #eb1145;
 `
-
-function Header({ user }: any) {
-	return (
-		<HeaderContainer>
-			<Logo to='/'>Aparatchi</Logo>
-			<Navbar>
-				<Items>
-					{user ? (
-						<Item>
-							<form action='/auth/logout' method='POST'>
-								<Button type='submit'>{user.name} خروج از حساب</Button>
-							</form>
-						</Item>
-					) : (
-						<>
-							<Item>
-								<Link to='/login'>
-									ورود <IoLogInOutline />
-								</Link>
-							</Item>
-							<Item>
-								<Link to='/register'>
-									ثبت نام <AiOutlineUserAdd />
-								</Link>
-							</Item>
-						</>
-					)}
-				</Items>
-			</Navbar>
-		</HeaderContainer>
-	)
-}
-
-export default Header
