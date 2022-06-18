597c6ab0bb523f395099c951603e81435be6d1e0
ijbrave <ssjavad00@gmail.com>
2022-06-18 11:45:17 +0430

    feat(constants): add thumbnailUrl

--- frontend/app/constants/movie.ts
+++ frontend/app/constants/movie.ts
@@ -1,2 +1,3 @@
 export const movieBaseUrl = 'https://image.tmdb.org/t/p/original'
 export const IMDbUrl = 'https://www.imdb.com/title/'
+export const thumbnailUrl = 'https://image.tmdb.org/t/p/w500'
